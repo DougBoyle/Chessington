0c861d4a77932b6f6a82072bdc6d8d8263a40809
emilies03 <estone97@gmail.com>
2020-07-13 14:15:19 +0100

    Rook can't move if piece infront

--- Chessington.GameEngine/Pieces/Moves.cs
+++ Chessington.GameEngine/Pieces/Moves.cs
@@ -1,46 +1,91 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Chessington.GameEngine.Pieces {
-    public class Moves {
-        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition) {
+namespace Chessington.GameEngine.Pieces
+{
+    public class Moves
+    {
+        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition,Player currentPlayer)
+        {
             List<Square> availableMoves = new List<Square>();
-            foreach (var index in Enumerable.Range(0, GameSettings.BoardSize)) {
-                if (currentPosition.Col != index) {
-                    availableMoves.Add(new Square(currentPosition.Row, index));
-                }
+            var directions = new int[] {-1, 1};
+            foreach (var direction in directions)
+            {
+                var limit = direction == 1 ? GameSettings.BoardSize - currentPosition.Col : currentPosition.Col + 1;
+                for (var index = 1; index < limit; index++)
+                {
+                    Square targetSquare = new Square(currentPosition.Row, currentPosition.Col + index*direction);
+                    if (board.IsSquareEmpty(targetSquare))
+                    {
+                        availableMoves.Add(targetSquare);
+                    }
+                    else
+                    {
+                        Player piecePlayer = board.GetPiece(targetSquare).Player;
+                        if (piecePlayer != currentPlayer)
+                        {
+                            availableMoves.Add(targetSquare);
+                        }
 
-                if (currentPosition.Row != index) {
-                    availableMoves.Add(new Square(index, currentPosition.Col));
+                        break;
+                    }
+                }
+            }
+            foreach (var direction in directions)
+            {
+                var limit = direction == 1 ? GameSettings.BoardSize  - currentPosition.Row : currentPosition.Row +1;
+                for (var index = 1; index <limit; index++)
+                {
+                    Square targetSquare = new Square(currentPosition.Row + index*direction, currentPosition.Col);
+                    if (board.IsSquareEmpty(targetSquare))
+                    {
+                        availableMoves.Add(targetSquare);
+                    }
+                    else
+                    {
+                        Player piecePlayer = board.GetPiece(targetSquare).Player;
+                        if (piecePlayer != currentPlayer)
+                        {
+                            availableMoves.Add(targetSquare);
+                        }
+
+                        break;
+                    }
                 }
             }
 
             return availableMoves;
         }
 
-        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition) {
+
+        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition)
+        {
             List<Square> availableMoves = new List<Square>();
 
-            for (var index = 1; index <= currentPosition.Col && index <= currentPosition.Row; index++) {
+            for (var index = 1; index <= currentPosition.Col && index <= currentPosition.Row; index++)
+            {
                 availableMoves.Add(new Square(currentPosition.Row - index, currentPosition.Col - index));
             }
 
             for (var index = 1;
                 index <= currentPosition.Col && index < GameSettings.BoardSize - currentPosition.Row;
-                index++) {
+                index++)
+            {
                 availableMoves.Add(new Square(currentPosition.Row + index, currentPosition.Col - index));
             }
 
             for (var index = 1;
                 index < GameSettings.BoardSize - currentPosition.Col &&
                 index < GameSettings.BoardSize - currentPosition.Row;
-                index++) {
+                index++)
+            {
                 availableMoves.Add(new Square(currentPosition.Row + index, currentPosition.Col + index));
             }
 
             for (var index = 1;
                 index < GameSettings.BoardSize - currentPosition.Col && index <= currentPosition.Row;
-                index++) {
+                index++)
+            {
                 availableMoves.Add(new Square(currentPosition.Row - index, currentPosition.Col + index));
             }
 

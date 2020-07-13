7901653ecf77039bc6188eaaed9b7081083efe8e
DougBoyle <dab80@cam.ac.uk>
2020-07-13 14:59:10 +0100

    Generate next square along beam

--- Chessington.GameEngine/Pieces/Moves.cs
+++ Chessington.GameEngine/Pieces/Moves.cs
@@ -6,64 +6,41 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Moves
     {
-        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition,Player currentPlayer)
-        {
+        public static List<Square> GetBeamMoves(Board board, Square currentPosition, 
+            Player currentPlayer, Func<Square, Square> iterator) {
             List<Square> availableMoves = new List<Square>();
-            var directions = new int[] {-1, 1};
-            foreach (var direction in directions)
-            {
-                var limit = direction == 1 ? GameSettings.BoardSize - currentPosition.Col : currentPosition.Col + 1;
-                for (var index = 1; index < limit; index++)
-                {
-                    Square targetSquare = new Square(currentPosition.Row, currentPosition.Col + index*direction);
-                    if (board.IsEmptyOrOpponent(targetSquare, currentPlayer)) availableMoves.Add(targetSquare);
-                    if (!board.IsSquareEmpty(targetSquare)) break;
-                }
-            }
-            foreach (var direction in directions)
-            {
-                var limit = direction == 1 ? GameSettings.BoardSize  - currentPosition.Row : currentPosition.Row +1;
-                for (var index = 1; index <limit; index++)
-                {
-                    Square targetSquare = new Square(currentPosition.Row + index*direction, currentPosition.Col);
-                    if (board.IsEmptyOrOpponent(targetSquare, currentPlayer)) availableMoves.Add(targetSquare);
-                    if (!board.IsSquareEmpty(targetSquare)) break;
-                }
+            while ((currentPosition = iterator.Invoke(currentPosition)).IsValid()) {
+                if (board.IsEmptyOrOpponent(currentPosition, currentPlayer)) availableMoves.Add(currentPosition);
+                if (!board.IsSquareEmpty(currentPosition)) break;
             }
 
             return availableMoves;
         }
+        
+        
+        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition,Player currentPlayer)
+        {
+            var availableMoves = GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row + 1, square.Col));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row - 1, square.Col)));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row, square.Col + 1)));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row, square.Col - 1)));
+            return availableMoves;
+        }
 
-
-        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition, Player currentPlayer){
-            List<Square> availableMoves = new List<Square>();
-            var directions = new int[] {-1, 1};
-            foreach (var direction in directions)
-            {
-                var limit = direction == 1 ? 
-                    Math.Min(GameSettings.BoardSize - currentPosition.Col, GameSettings.BoardSize - currentPosition.Row)
-                    : Math.Min(currentPosition.Col, currentPosition.Row) + 1;
-                for (var index = 1; index < limit; index++)
-                {
-                    Square targetSquare = new Square(currentPosition.Row + index*direction, 
-                        currentPosition.Col + index*direction);
-                    if (board.IsEmptyOrOpponent(targetSquare, currentPlayer)) availableMoves.Add(targetSquare);
-                    if (!board.IsSquareEmpty(targetSquare)) break;
-                }
-            }
-            foreach (var direction in directions)
-            {
-                var limit = direction == 1 ? 
-                    Math.Min(GameSettings.BoardSize - currentPosition.Col, currentPosition.Row + 1) 
-                    : Math.Min(currentPosition.Col + 1, GameSettings.BoardSize - currentPosition.Row);
-                for (var index = 1; index < limit; index++)
-                {
-                    Square targetSquare = new Square(currentPosition.Row - index*direction, 
-                        currentPosition.Col + index*direction);
-                    if (board.IsEmptyOrOpponent(targetSquare, currentPlayer)) availableMoves.Add(targetSquare);
-                    if (!board.IsSquareEmpty(targetSquare)) break;
-                }
-            }
+        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition, Player currentPlayer)
+        {
+            var availableMoves = GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row + 1, square.Col + 1));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row + 1, square.Col - 1)));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row - 1, square.Col + 1)));
+            availableMoves.AddRange(GetBeamMoves(board, currentPosition, currentPlayer, 
+                square => new Square(square.Row - 1, square.Col - 1)));
             return availableMoves;
         }
     }

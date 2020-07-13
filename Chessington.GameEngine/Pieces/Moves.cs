c904eb54416b304cbf1bf5b328d330cb8b9ea66f
DougBoyle <dab80@cam.ac.uk>
2020-07-13 14:46:49 +0100

    Captial functions and semicolons

--- Chessington.GameEngine/Pieces/Moves.cs
+++ Chessington.GameEngine/Pieces/Moves.cs
@@ -3,46 +3,47 @@ using System.Linq;
 
 namespace Chessington.GameEngine.Pieces {
     public class Moves {
-        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition)
-        {
+        public static IEnumerable<Square> GetLateralMoves(Board board, Square currentPosition) {
             List<Square> availableMoves = new List<Square>();
-            foreach (var index in Enumerable.Range(0,GameSettings.BoardSize))
-            {
-                if (currentPosition.Col != index)
-                {
+            foreach (var index in Enumerable.Range(0, GameSettings.BoardSize)) {
+                if (currentPosition.Col != index) {
                     availableMoves.Add(new Square(currentPosition.Row, index));
                 }
 
-                if (currentPosition.Row != index)
-                {
-                    availableMoves.Add(new Square(index,currentPosition.Col));
+                if (currentPosition.Row != index) {
+                    availableMoves.Add(new Square(index, currentPosition.Col));
                 }
             }
+
             return availableMoves;
         }
-        
-        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition)
-        {
+
+        public static IEnumerable<Square> GetDiagonalMoves(Board board, Square currentPosition) {
             List<Square> availableMoves = new List<Square>();
-            
-            for(var index=1;index<=currentPosition.Col && index<=currentPosition.Row;index ++)
-            {
-                availableMoves.Add(new Square(currentPosition.Row-index, currentPosition.Col-index));
+
+            for (var index = 1; index <= currentPosition.Col && index <= currentPosition.Row; index++) {
+                availableMoves.Add(new Square(currentPosition.Row - index, currentPosition.Col - index));
             }
-            for (var index=1;index<=currentPosition.Col && index<GameSettings.BoardSize-currentPosition.Row;index ++)
-            {
-                availableMoves.Add(new Square(currentPosition.Row+index, currentPosition.Col-index));
+
+            for (var index = 1;
+                index <= currentPosition.Col && index < GameSettings.BoardSize - currentPosition.Row;
+                index++) {
+                availableMoves.Add(new Square(currentPosition.Row + index, currentPosition.Col - index));
             }
-            for (var index=1;
-                index<GameSettings.BoardSize-currentPosition.Col && index<GameSettings.BoardSize-currentPosition.Row;
-                index ++)
-            {
-                availableMoves.Add(new Square(currentPosition.Row+index, currentPosition.Col+index));
+
+            for (var index = 1;
+                index < GameSettings.BoardSize - currentPosition.Col &&
+                index < GameSettings.BoardSize - currentPosition.Row;
+                index++) {
+                availableMoves.Add(new Square(currentPosition.Row + index, currentPosition.Col + index));
             }
-            for (var index=1;index<GameSettings.BoardSize-currentPosition.Col && index<=currentPosition.Row; index++)
-            {
+
+            for (var index = 1;
+                index < GameSettings.BoardSize - currentPosition.Col && index <= currentPosition.Row;
+                index++) {
                 availableMoves.Add(new Square(currentPosition.Row - index, currentPosition.Col + index));
             }
+
             return availableMoves;
         }
     }

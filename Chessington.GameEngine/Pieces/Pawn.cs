8fe226dfd655b2e5a3403ea3d8b0e318754a5e9d
DougBoyle <dab80@cam.ac.uk>
2020-07-14 09:14:05 +0100

    Refactor pawn moves

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -2,46 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Chessington.GameEngine.Pieces
-{
-    public class Pawn : Piece
-    {
-        public Pawn(Player player) 
+namespace Chessington.GameEngine.Pieces {
+    public class Pawn : Piece {
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board) {
             List<Square> available = new List<Square>();
             Square here = board.FindPiece(this);
-            if (Player == Player.White)
-            {
-                Square targetMove = new Square(here.Row - 1, here.Col);
-                if (targetMove.IsValid() && board.IsSquareEmpty(targetMove))
-                {
-                    Square targetMove2 = new Square(here.Row - 2, here.Col);
-                    if (here.Row != 0)
-                    {
-                        available.Add(targetMove);
-                    }
-                    if (here.Row == GameSettings.BoardSize - 2 && board.IsSquareEmpty(targetMove2))
-                    {
-                        available.Add(targetMove2);
-                    }
-                }
-            } else {
-                Square targetMove = new Square(here.Row +1, here.Col);
-                if (targetMove.IsValid() && board.IsSquareEmpty(targetMove))
-                {
-                    Square targetMove2 = new Square(here.Row +2, here.Col);   
-                    if (here.Row != GameSettings.BoardSize - 1)
-                    {
-                        available.Add(targetMove);
-                    }
-                    if (here.Row == 1 && board.IsSquareEmpty(targetMove2))
-                    {
-                        available.Add(targetMove2);
-                    }
+
+            var direction = Player == Player.White ? -1 : 1;
+            var homeRow = Player == Player.White ? GameSettings.BoardSize - 2 : 1;
+
+            Square targetMove = new Square(here.Row + direction, here.Col);
+            if (targetMove.IsValid() && board.IsSquareEmpty(targetMove)) {
+                available.Add(targetMove);
+                Square targetMove2 = new Square(here.Row + 2 * direction, here.Col);
+                if (here.Row == homeRow && board.IsSquareEmpty(targetMove2)) {
+                    available.Add(targetMove2);
                 }
             }
+
             return available;
         }
     }
